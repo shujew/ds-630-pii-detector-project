5c10295317a9f8474ea009b3e6f7f8b46a235ff7
Shuaib Jewon <shuaib.jewon@slader.com>
2021-11-17 18:08:26 -0500

    reverting some changes to make it work on macOS

--- src/parsers/detectors/PIIAnalyzerDetector.py
+++ src/parsers/detectors/PIIAnalyzerDetector.py
@@ -1,5 +1,4 @@
 import csv
-import os
 import pandas as pd
 import tempfile
 import sys
@@ -12,38 +11,23 @@ from parsers.detectors.DetectorInterface import DetectorInterface
 # to analyze big fields
 csv.field_size_limit(min(sys.maxsize, 2147483646))
 
-
 class PIIAnalyzerDetector(DetectorInterface):
     """
     Detector for PIIAnalyzer
     """
 
     def extract_pii_from_text(self, text):
-        # create and write text to temp file
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        tmp.write(str.encode(text, encoding='utf-8'))
-        # do analysis
-        piianalyzer = PiiAnalyzer(tmp.name)
-        results = piianalyzer.analysis()
-        # delete temp file
-        tmp.close()
-        os.unlink(tmp.name)
-
-        return results
+        with tempfile.NamedTemporaryFile() as tmp:
+            with open(tmp.name, 'w') as f:
+                f.write(text)
+            piianalyzer = PiiAnalyzer(tmp.name)
+            return piianalyzer.analysis()
 
     def extract_pii_from_df(self, df):
-        # create and write text to temp file
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        df.to_csv(tmp)
-        # do analysis
-        piianalyzer = PiiAnalyzer(tmp.name)
-        results = piianalyzer.analysis()
-        # delete temp file
-        tmp.close()
-        os.unlink(tmp.name)
-
-        return results
-
+        with tempfile.NamedTemporaryFile() as tmp:
+            df.to_csv(tmp)
+            piianalyzer = PiiAnalyzer(tmp.name)
+            return piianalyzer.analysis()
 
 class PiiAnalyzer(object):
     """
@@ -51,7 +35,6 @@ class PiiAnalyzer(object):
     in the library and it wouldn't work on python 3.7
     Adapted from https://gitlab.math.ubc.ca/tomyerex/piianalyzer/-/blob/master/piianalyzer/analyzer.py
     """
-
     def __init__(self, filepath):
         self.filepath = filepath
         self.parser = CommonRegex()
@@ -79,8 +62,7 @@ class PiiAnalyzer(object):
                 data.extend(row)
                 for text in row:
                     emails.extend(self.parser.emails(text))
-                    phone_numbers.extend(
-                        self.parser.phones("".join(text.split())))
+                    phone_numbers.extend(self.parser.phones("".join(text.split())))
                     street_addresses.extend(self.parser.street_addresses(text))
                     credit_cards.extend(self.parser.credit_cards(text))
                     ips.extend(self.parser.ips(text))
@@ -94,12 +76,12 @@ class PiiAnalyzer(object):
                 organizations.append(title)
 
         return {
-            'PERSON': people,
-            'LOCATION': locations,
+            'PERSON': people, 
+            'LOCATION': locations, 
             'ORGANIZATION': organizations,
-            'EMAIL_ADDRESS': emails,
-            'PHONE_NUMBER': phone_numbers,
+            'EMAIL_ADDRESS': emails, 
+            'PHONE_NUMBER': phone_numbers, 
             'LOCATION': street_addresses,
-            'CREDIT_CARD': credit_cards,
+            'CREDIT_CARD': credit_cards, 
             'IP_ADDRESS': ips
         }
